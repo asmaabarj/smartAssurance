a270f528e3b9684cecc2eef275b90d95a0dcf1ae
asmaabarj <asmaabarj5@gmail.com>
2025-03-10 11:06:13 +0000

    réalidation du service policy

--- policy-service/src/main/java/com/insurance/policy/controller/PolicyController.java
+++ policy-service/src/main/java/com/insurance/policy/controller/PolicyController.java
@@ -1,22 +1,13 @@
 package com.insurance.policy.controller;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.insurance.policy.dto.PolicyDTO;
 import com.insurance.policy.model.Policy;
 import com.insurance.policy.service.PolicyService;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/policies")
--- policy-service/src/main/java/com/insurance/policy/dto/CustomerDTO.java
+++ policy-service/src/main/java/com/insurance/policy/dto/CustomerDTO.java
@@ -1,8 +1,12 @@
 package com.insurance.policy.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDTO {
     private Long id;
     private String nom;
